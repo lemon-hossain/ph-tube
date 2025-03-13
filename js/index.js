8ccdc7b1ac2cdaa7754c37cb9086deb1510f051b
lemonhossainofficial <lemonhossainofficial@gmail.com>
2025-03-13 15:41:14 +0600

    all requirement done

--- js/index.js
+++ js/index.js
@@ -5,6 +5,15 @@ function loadCategories() {
     .then((data) => displayCategories(data.categories));
 }
 
+// load Categories video
+const loadCategoriesVideo = (id) => {
+  showLoader();
+  const url = `https://openapi.programming-hero.com/api/phero-tube/category/${id}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayVideos(data.category));
+};
+
 function displayCategories(categories) {
   const categoriesContainer = document.getElementById("categoriesContainer");
   // for of loop
@@ -12,7 +21,7 @@ function displayCategories(categories) {
     // create elements
     const div = document.createElement("div");
     div.innerHTML = `
-              <button class="btn btn-sm bg-[#FF1F3D]  text-white hover:bg-bl text-[#FF1F3D] font-semibold text-[18px]">${cat.category}</button>
+              <button onclick="loadCategoriesVideo(${cat.category_id})" class="btn btn-sm font-semibold text-[18px] hover:bg-[#FF1F3D] hover:text-white">${cat.category}</button>
 
         `;
     categoriesContainer.append(div);
@@ -20,36 +29,54 @@ function displayCategories(categories) {
 }
 
 // display videos
-function loadVideos() {
-  fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+function loadVideos(searchText = "") {
+  showLoader();
+  fetch(
+    `https://openapi.programming-hero.com/api/phero-tube/videos?title=${searchText}`
+  )
     .then((res) => res.json())
     .then((data) => displayVideos(data.videos));
 }
 
-// {
-//     "category_id": "1001",
-//     "video_id": "aaab",
-//     "thumbnail": "https://i.ibb.co/QPNzYVy/moonlight.jpg",
-//     "title": "Midnight Serenade",
-//     "authors": [
-//         {
-//             "profile_picture": "https://i.ibb.co/fDbPv7h/Noha.jpg",
-//             "profile_name": "Noah Walker",
-//             "verified": false
-//         }
-//     ],
-//     "others": {
-//         "views": "543K",
-//         "posted_date": ""
-//     },
-//     "description": "'Midnight Serenade' by Noah Walker is a soulful journey into the depths of the night, capturing the mystique and allure of a moonlit evening. With 543K views, this song brings together tender melodies and evocative lyrics, making it a favorite among listeners seeking a contemplative yet uplifting experience. Immerse yourself in this musical masterpiece and feel the calm embrace of the night."
-// }
+// loadVideoDetails
+const loadVideoDetails = (videoId) => {
+  console.log(videoId);
+  const url = `https://openapi.programming-hero.com/api/phero-tube/video/${videoId}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayLoadVideoDetails(data.video));
+};
+
+// Display load Video Details
+const displayLoadVideoDetails = (video) => {
+  document.getElementById("showModal").showModal();
+
+  const modalTitleContainr = document.getElementById("modalTitleContainr");
+  const div = document.createElement("div");
+  div.innerHTML = `
+  <h2>${video.title}</h2>
+  `;
+  modalTitleContainr.append(div);
+  // document.getElementById("videoDetails").showModal();
+};
 
 const displayVideos = (videos) => {
   const videoContainer = document.getElementById("videoContainer");
+  videoContainer.innerHTML = "";
+  if (videos.length == 0) {
+    videoContainer.innerHTML = `
+  <div class="col-span-full flex flex-col items-center">
+          <img class="w-[120px]" src="images/Icon.png" alt="" />
+          <h2 class="font-bold text-3xl mt-9 text-center">
+            Oops!! Sorry, There is no <br />content here
+          </h2>
+        </div>
+  `;
+    hideLoader();
+    return;
+  }
+
   videos.forEach((video) => {
-    console.log(video);
-    
     const videoCards = document.createElement("div");
     videoCards.innerHTML = `
      <div class="card bg-base-100">
@@ -69,18 +96,45 @@ const displayVideos = (videos) => {
           <h2 class="card-title font-bold">${video.title}</h2>
         </div>
         <div class="flex gap-3 pt-3 items-center">
-          <p>${video.authors[0].profile_name}</p>
-          <img class="w-5 h-5" src="https://img.icons8.com/?size=96&id=SRJUuaAShjVD&format=png" alt="">
+          <p>
+          ${video.authors[0].profile_name}
+          <div>
+          ${
+            video.authors[0].verified == true
+              ? `<img class="w-5 h-5" src="https://img.icons8.com/?size=96&id=SRJUuaAShjVD&format=png" alt="">`
+              : ``
+          }</div>
+          </p>
+          
         </div>
         <p class="pt-3">${video.others.views}</p>
+        <div class="pt-5">
+        <button onclick=loadVideoDetails("${
+          video.video_id
+        }") class="btn btn-block">Show Details</button>
+        </div>
       </div>
-
-
-
     `;
     videoContainer.append(videoCards);
   });
+  hideLoader();
 };
 
-loadVideos();
+// search
+document.getElementById("search").addEventListener("keyup", (event) => {
+  const input = event.target.value;
+  loadVideos(input);
+});
+
+// loader
+const showLoader = () => {
+  document.getElementById("loader").classList.remove("hidden");
+  document.getElementById("videoContainer").classList.add("hidden");
+};
+
+const hideLoader = () => {
+  document.getElementById("loader").classList.add("hidden");
+  document.getElementById("videoContainer").classList.remove("hidden");
+};
+
 loadCategories();
